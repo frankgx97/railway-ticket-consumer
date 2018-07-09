2dfcad08c62938cd13347e7b25e93602b76365fa
frank <i@nyan.im>
2018-07-09 14:41:42 +0800

    init models

---
+++ src/test/java/cn/guoduhao/TicketSystemConsumer/TicketSystemConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.guoduhao.TicketSystemConsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketSystemConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
