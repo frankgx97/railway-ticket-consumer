f70a7afb9e3f2273b422e76d6e491686d2fc1f69
XinyaoTian <31195026+XinyaoTian@users.noreply.github.com>
2018-07-13 15:35:18 +0800

    实现分段购票 北京——上海 线路

--- src/test/java/cn/guoduhao/TicketSystemConsumer/TicketSystemConsumerApplicationTests.java
+++ src/test/java/cn/guoduhao/TicketSystemConsumer/TicketSystemConsumerApplicationTests.java
@@ -1,13 +1,26 @@
 package cn.guoduhao.TicketSystemConsumer;
 
+import cn.guoduhao.TicketSystemConsumer.Services.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TicketSystemConsumerApplicationTests {
+
+	@Autowired
+	OrderService orderService;
+
+	@Test
+	@Bean
+	public void byRemanentTicketTest(){
+
+	}
+
 
 	@Test
 	public void contextLoads() {
